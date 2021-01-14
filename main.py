ee42765cc3bcdf005afd09f7d8a45cad43e96967
Ethan Groat <ethantgroat@gmail.com>
2021-01-14 09:42:16 -0800

    Set top p to 1, other small settings tweaks

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ USER = os.getenv('USER')
 AI_NAME = 'Logan'
 CHAT_MODEL = 'gpt2-medium'
 SAMPLING_METHOD = 'top-p-nucleus-sampling'  # 'greedy'  # 'beam-search'  # 'top-k-sampling'
-LENGTH_VARIANCE = 16
+LENGTH_VARIANCE = 32
 PROMPT_REPEAT_CHANCE = 0.08
 GPT_RESCUE_CHANCE = 0.95
 CUSTOM_CHAT_SETTINGS = {
   "do_sample": True,
   "early_stopping": True,
   # "num_beams": 4,
-  "temperature": 0.4,
+  "temperature": 0.35,
   # "top_k": 80,
-  "top_p": 0.8,
+  "top_p": 1,
   # "repetition_penalty": 1,
   # "length_penalty": 1,
   # "no_repeat_ngram_size": 2,
